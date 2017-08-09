1131172ac316f690ad030f3285712ea3bc279a1b
James Fernandez <jffernan@comcast.net>
2017-08-09 18:44:27 +0000

    Tested signup => works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,13 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: "User successfully created"
+    else
+      redirect_to '/signup', notice: "Please try again!"
+    end
   end
 
   def update
