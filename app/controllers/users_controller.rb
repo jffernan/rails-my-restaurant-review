a9ff3e605f1300983818f62ba6a439a383f73374
James Fernandez <jffernan@comcast.net>
2017-08-09 18:15:16 +0000

    coding out user mvc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  include ApplicationHelper
+
+  def index
+  end
+
+  def show
+    if logged_in?
+      render :show
+    else
+      redirect_to '/'
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def logged_in?
+
+  end
+
+  def current_user
+
+  end
+
 end
