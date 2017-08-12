528574ca81b12a3ee99a5b18de01f08bfa13f057
James Fernandez <jffernan@comcast.net>
2017-08-12 16:06:09 +0000

    generated mvc crud for Review model domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'reviews/index'
+
+  get 'reviews/show'
+
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'reviews/edit'
+
+  get 'reviews/update'
+
+  get 'reviews/destroy'
+
   root 'users#home'
 
   resources :users
