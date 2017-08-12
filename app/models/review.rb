656140827d4ede897f60b3707f8e70102dfafd6c
James Fernandez <jffernan@comcast.net>
2017-08-12 23:31:55 +0000

    Fixed reviews mvc crud + added links throughout

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,15 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
-  validates_presence_of :content, :rating
+  validates_presence_of :content, :rating, :cuisine, :review_date
+
+  def restaurant_name
+    self.restaurant.name #class method to access restaurant name
+  end
+
+  #def user_email
+    #self.user.email #class method to access user email
+  #end
 
   def self.alphabetical_order #ActiveRecord method to alphabetize lists
     Review.order(rest_name: :asc) #Alphabetize by Restaurant name
