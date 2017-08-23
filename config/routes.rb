3ba3e71c5578837c32f77b33a5f2e7c430f111c0
James Fernandez <jffernan@comcast.net>
2017-08-23 12:42:53 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [:index, :show]
   end
   
-  get '/reviews/top_reviews', to: 'reviews#index', as: :top_reviews
+  get '/reviews/top_reviews', to: 'reviews#top_reviews', as: :top_reviews
 
 end
