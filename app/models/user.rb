f675d743ef4f274d304babd1a213fce68c3065fd
James Fernandez <jffernan@comcast.net>
2017-08-27 17:09:39 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
+  extend FriendlyId
+  friendly_id :email
+  
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
