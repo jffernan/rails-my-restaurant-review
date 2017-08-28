d2fa76501c13818c046decde6d4fe4284cdeed19
James Fernandez <jffernan@comcast.net>
2017-08-28 13:25:07 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper #all controllers have helper access
 
-  before_action :require_login, except: [:active_admin] #filter prevernt unauth. access/use
+  before_action :require_login, except: :admin #filter prevernt unauth. access/use
 
   private
   def require_login #before action filter confirm logged-in user
