1e8bfdf79a9a03a21658c4f50da3e2885c7b4076
J Fernandez <jffernan@comcast.net>
2017-09-02 20:31:34 +0000

    update seed.rb db file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-5.times do |n|
+25.times do |n|
   email = Faker::Internet.unique.safe_email
   password = "password"
   password_confirmation = "password"
@@ -7,12 +7,9 @@
                password_confirmation: password)
   restaurant_name = Faker::Company.name
   content =  Faker::Lorem.sentence
-  cuisine_ids = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
   cuisine_name = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
-  cuisine = "Burgers"
-  date_visited = Faker::Date.backward(185)
+  date_visited = Faker::Date.backward(125)
   rating = ["Excellent", "Good", "Average", "Poor"].sample
-
   Review.create!(user: user,
                 restaurant_name: restaurant_name,
                 content: content,
@@ -20,95 +17,7 @@
                 rating: rating)
 end
 
-5.times do |n|
-  email = Faker::Internet.unique.safe_email
-  password = "password"
-  password_confirmation = "password"
-  user = User.create!(email:                 email,
-               password:              password,
-               password_confirmation: password)
-  restaurant_name = Faker::Company.name
-  content =  Faker::Lorem.sentence
-  cuisine_ids = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
-  #cuisine = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
-  cuisine = "Pizza"
-  date_visited = Faker::Date.backward(185)
-  rating = ["Excellent", "Good", "Average", "Poor"].sample
-
-  Review.create!(user: user,
-                restaurant_name: restaurant_name,
-                content: content,
-                date_visited: date_visited,
-                rating: rating)
-end
-
-5.times do |n|
-  email = Faker::Internet.unique.safe_email
-  password = "password"
-  password_confirmation = "password"
-  user = User.create!(email:                 email,
-               password:              password,
-               password_confirmation: password)
-  restaurant_name = Faker::Company.name
-  content =  Faker::Lorem.sentence
-  cuisine_ids = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
-  #cuisine = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
-  cuisine = "Deli"
-  date_visited = Faker::Date.backward(185)
-  rating = ["Excellent", "Good", "Average", "Poor"].sample
-
-  Review.create!(user: user,
-                restaurant_name: restaurant_name,
-                content: content,
-                date_visited: date_visited,
-                rating: rating)
-end
-
-5.times do |n|
-  email = Faker::Internet.unique.safe_email
-  password = "password"
-  password_confirmation = "password"
-  user = User.create!(email:                 email,
-               password:              password,
-               password_confirmation: password)
-  restaurant_name = Faker::Company.name
-  content =  Faker::Lorem.sentence
-  cuisine_ids = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
-  #cuisine = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
-  cuisine = "Upscale"
-  date_visited = Faker::Date.backward(185)
-  rating = ["Excellent", "Good", "Average", "Poor"].sample
-
-  Review.create!(user: user,
-                restaurant_name: restaurant_name,
-                content: content,
-                date_visited: date_visited,
-                rating: rating)
-end
-
-5.times do |n|
-  email = Faker::Internet.unique.safe_email
-  password = "password"
-  password_confirmation = "password"
-  user = User.create!(email:                 email,
-               password:              password,
-               password_confirmation: password)
-  restaurant_name = Faker::Company.name
-  content =  Faker::Lorem.sentence
-  cuisine_ids = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
-  #cuisine = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
-  cuisine = "Teriyaki"
-  date_visited = Faker::Date.backward(185)
-  rating = ["Excellent", "Good", "Average", "Poor"].sample
-
-  Review.create!(user: user,
-                restaurant_name: restaurant_name,
-                content: content,
-                date_visited: date_visited,
-                rating: rating)
-end
-
-Cuisine.create(name: 'Italian')
-Cuisine.create(name: 'Greek')
-Cuisine.create(name: 'Burgers')
-Cuisine.create(name: 'Buffet')
+Cuisine.create!(name: 'Pizza')
+Cuisine.create!(name: 'Deli')
+Cuisine.create!(name: 'Burgers')
+Cuisine.create!(name: 'Buffet')
