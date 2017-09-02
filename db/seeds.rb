9b564216ff7bec445bfaf39e21bfdf70a39be390
J Fernandez <jffernan@comcast.net>
2017-09-02 22:31:16 +0000

    Fixed rake db:seed for cuisine

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,11 @@
   restaurant_name = Faker::Company.name
   content =  Faker::Lorem.sentence
   cuisine_name = ["Upscale", "American", "Fast Food", "Chinese", "Mexican", "Steak"].sample
+  review_cuisine = Cuisine.find_or_create_by :name => cuisine_name
   date_visited = Faker::Date.backward(125)
   rating = ["Excellent", "Good", "Average", "Poor"].sample
   Review.create!(user: user,
+                :cuisines => [review_cuisine],
                 restaurant_name: restaurant_name,
                 content: content,
                 date_visited: date_visited,
