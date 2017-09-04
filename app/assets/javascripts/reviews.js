4ddff9235800fb5bca4ad93cf5b40844c01dd91a
James Fernandez <jffernan@comcast.net>
2017-09-04 14:30:40 -0700

    Update reviews.js

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("a.load_reviews").on("click", function(e){
  //Send AJAX HIGH-LEVEL get request
   $.get(this.href).success(function(response) {				
-    $(“div.reviews”).html(response)				
+    $("div.reviews").html(response)				
   });
 //Load response into HTML of page
    e.preventDefault();
