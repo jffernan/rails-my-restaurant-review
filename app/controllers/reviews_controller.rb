e5f4f14cf79e89526ffc1d2fa84d97fb95f318a9
J Fernandez <jffernan@comcast.net>
2017-09-04 01:41:28 +0000

    Done w/ AJAX get request for rest reviews on rest directory

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,11 +6,11 @@ class ReviewsController < ApplicationController
     @users = User.all.alphabetical_order.page(params[:page]) #need for user filter
     if params[:user_id]
       @reviews = User.find(params[:user_id]).reviews.page(params[:page])#Show all reviews for link_to specific user
+    elsif params[:user]
+      @reviews = Review.by_user(params[:user]).page(params[:page]) #Filter box to search reviews by user
     elsif params[:restaurant_id]
       @reviews = Restaurant.find(params[:restaurant_id]).reviews.page(params[:page])#Show all reviews for link_to specific restaurant
       render 'reviews/index', :layout => false
-    elsif params[:user]
-      @reviews = Review.by_user(params[:user]).page(params[:page]) #Filter box to search reviews by user
     elsif params[:rating]
       if params[:rating] == "Excellent"
         @reviews = Review.top_reviews(params[:rating]).order_by_date_visited.page(params[:page])
