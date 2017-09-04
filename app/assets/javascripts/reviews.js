d52e85150a22ac5478070b5577aaf6593b93a72f
J Fernandez <jffernan@comcast.net>
2017-09-04 21:47:15 +0000

    Finished AJAX get requests for both show pages

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,10 +1,14 @@
-$(document).ready(function() {
-  $("a.load_reviews").on("click", function(e){
+$(function() {
+  $("a.load_reviews").on("click", function(e) {
  //Send AJAX HIGH-LEVEL get request
-  $.get(this.href).success(function(response) {				
-    $(“div.reviews”).html(response)				
-  });
-//Load response into HTML of page
+  $.get(this.href).success(function(json) {
+    var $ol = $("div.reviews ol")
+    $ol.html("")
+    json.forEach(function(review) {
+      $ol.append("<li>" + review + "</li>");
+    })
+  })
+  //Load response into HTML of page
    e.preventDefault();
  })
 })
