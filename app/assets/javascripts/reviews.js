ac03ce8292a93b938a88b334f255ed8d0f1b5be9
J Fernandez <jffernan@comcast.net>
2017-09-06 21:47:28 +0000

    Fixing event handler for 'Next' button to show name correctly

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -30,8 +30,8 @@ $(function () {
   $(".js-next").on("click", function() {
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
     $.get("/reviews/" + nextId + ".json", function(data) {
-      var review = data;
-      $(".restaurantName").text(review["restaurant"]["name"]);
+      var review = data; //how to create link_to in js
+      $(".restaurantName").text("Restaurant Name: " + review["restaurant"]["name"]);
       $(".reviewBody").text("Comments: " + '"' + review["content"] + '"');
       $(".reviewCuisines").text("Cuisines:" + review["cuisine"]["name"]);
       $(".reviewRating").text(review["rating"]);
