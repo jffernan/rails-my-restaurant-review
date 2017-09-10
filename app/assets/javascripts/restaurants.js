deb9b8fed7d9d2f84f1394dce7bec4a1d82e315c
J Fernandez <jffernan@comcast.net>
2017-09-10 23:22:34 +0000

    Fixing load_reviews on each show pages

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(function() {
   $("a.load_reviews").on("click", function(e){
 //Send AJAX HIGH-LEVEL get request
   $.get(this.href).success(function(json) {
-    var $li = $("div.reviews li")
-    $li.html(" ")
+    var $ul = $("div.reviews ul")
+    $ul.html(" ")
     json.forEach(function(review) {
-      $li.append("<li>" + review + "</li>");
+      $ul.append("<li>" + review + "</li>");
     });
   });
  //Load response into HTML of page
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,10 +2,10 @@ $(function() {
     $("a.load_reviews").on("click", function(e){
 //Send AJAX HIGH-LEVEL get request
   $.get(this.href).success(function(json) {
-    var $li = $("div.reviews li")
-    $li.html(" ")
+    var $ul = $("div.reviews ul")
+    $ul.html(" ")
     json.forEach(function(review) {
-      $li.append("<li>" + review + "</li>");
+      $ul.append("<li>" + review + "</li>");
     });
   });
  //Load response into HTML of page
