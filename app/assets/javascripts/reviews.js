fa04965c456a1cff6104cd5d877eafddfcf16b79
James Fernandez <jffernan@comcast.net>
2017-09-09 20:59:25 -0700

    fix <li> in load_reviews links

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -3,10 +3,10 @@ $(function() {
   $("a.load_reviews").on("click", function(e) {   
 //Send AJAX HIGH-LEVEL get request
   $.get(this.href).success(function(json) {
-    var $ol = $("div.reviews ol")
-    $ol.html(" ")
+    var $li = $("div.reviews li")
+    $li.html(" ")
     json.forEach(function(review) {
-      $ol.append("<li>" + review + "</li>");
+      $li.append("<li>" + review + "</li>");
     });
   });
  //Load response into HTML of page
