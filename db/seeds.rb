dca24a558436d8337b0939a4ea7753b84badbde8
J Fernandez <jffernan@comcast.net>
2017-09-12 17:21:39 +0000

    Added new Faker date to re-seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,18 +26,18 @@ end
   user = User.create!(email:                 email,
                password:              password,
                password_confirmation: password)
-  restaurant_name = ["McDonalds", "Burger King", "Wendy's", "Jack In The Box", "Subway", "Arby's"].sample
-  content =  Faker::Lorem.paragraph(3, true, 3)
-  cuisine_name = ["Burgers", "Sandwiches", "Fast Food"].sample
-  review_cuisine = Cuisine.find_or_create_by :name => cuisine_name
-  date_visited = Faker::Date.backward(90)
-  rating = ["Excellent", "Good", "Average", "Poor"].sample
-    3.times do |n|
+  3.times do |n|
+    restaurant_name = ["McDonalds", "Burger King", "Wendy's", "Jack In The Box", "Subway", "Arby's"].sample
+    content =  Faker::Lorem.paragraph(3, true, 3)
+    cuisine_name = ["Burgers", "Sandwiches", "Fast Food"].sample
+    review_cuisine = Cuisine.find_or_create_by :name => cuisine_name
+    date_visited = Faker::Date.backward(90)
+    rating = ["Excellent", "Good", "Average", "Poor"].sample
     Review.create!(user: user,
-                :cuisines => [review_cuisine],
-                restaurant_name: restaurant_name,
-                content: content,
-                date_visited: date_visited,
-                rating: rating)
-    end
+              :cuisines => [review_cuisine],
+              restaurant_name: restaurant_name,
+              content: content,
+              date_visited: date_visited,
+              rating: rating)
+  end
 end
