f72f64febf9dfaf0f1811238bcab8c75ec412d9e
James Fernandez <jffernan@comcast.net>
2017-09-13 22:25:08 -0700

    Change :class for link "load_reviews" to "load_more_reviews"

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,6 +1,6 @@
 //reviews/index.html.erb for 'Load Reviews' link on Show Page
 $(function() {
-  $("a.load_reviews").on("click", function(e) {
+  $("a.load_more_reviews").on("click", function(e) {
 //Send AJAX HIGH-LEVEL get request
   $.get(this.href).success(function(json) {
     var $ul = $("div.reviews ul")
