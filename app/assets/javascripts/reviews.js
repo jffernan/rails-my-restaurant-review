a184d8990b23ea230f3e05ae72060bee668b3d3e
J Fernandez <jffernan@comcast.net>
2017-09-15 00:52:38 +0000

    Refactor format of Cuisine name to match across all views on pages & renders

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -7,3 +7,19 @@ $(function() {
     e.preventDefault();
   });
 });
+
+class Review {
+  constructor(review) {
+    this.id = id;
+    this.restaurant_name = review.restaurant_name;
+    this.content = review.content;
+    this.rating = review.rating;
+    this.date_visited = review.date_visited;
+    this.user = review.user;
+    this.user_id = review.user_id;
+    this.restaurant_id = review.restaurant_id;
+  }
+   reviewComments() {
+    return "You commented about <b>"+this.restaurant_name+': "'+this.content+'."</b>';
+   }
+};
