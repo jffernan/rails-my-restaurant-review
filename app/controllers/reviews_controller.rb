673c10b271284e8256af98b7df2e84058268c447
James Fernandez <jffernan@comcast.net>
2017-09-15 16:45:21 -0700

    clarify #update comment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -45,7 +45,7 @@ class ReviewsController < ApplicationController
     if @review.save
       @review.restaurant_name = @review.restaurant_name.upcase_first
       render json: @review, status: 201 #Not go to Review show page
-      #redirect_to @review, success: "New review created!"
+      #NOT redirect_to @review, success: "New review created!"
     else
       flash.now[:alert] = "Save Review unsuccesful! Please try again!"
       render 'new'
@@ -54,7 +54,7 @@ class ReviewsController < ApplicationController
 
   def update
     if @review.update(review_params) #update a record that already exists, accepts hash containing attributes
-      redirect_to @review, notice: "Review updated!" #Goto Review show page
+      redirect_to @review, notice: "Review updated!" #Goto Review show page NOT render via $ AJAX POST
     else
       flash.now[:alert] = "Review Update unsuccesful! Please try again!"
       render 'edit'
