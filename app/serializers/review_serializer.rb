3e6c7e7221b10218990130691084e13692a365c5
James Fernandez <jffernan@comcast.net>
2017-09-15 12:55:24 -0700

    Added missing :updated_at attribute to Review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :restaurant_name, :content, :cuisines, :rating, :date_visited
+  attributes :id, :restaurant_name, :content, :cuisines, :rating, :date_visited, :updated_at
   belongs_to :user
   belongs_to :restaurant
 end
