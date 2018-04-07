f722eb21b03a335719d2a37ad066247d4d29b743
James Fernandez <jffernan@comcast.net>
2018-04-07 13:20:41 -0700

    get /logout not delete due to lack browser support

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   get '/auth/github/callback' => 'sessions#create' #must be name of provider
   get '/auth/failure', to: redirect('/')
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy' #get not delete due to lack of browser support
 
   resources :users, only: [:show] do #nested resource, parent: user => many reviews
     resources :reviews, only: [:index, :show, :new, :edit] #child: review LiMIT user to see ONLY another user's review(s)
